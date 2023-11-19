a96d2540a185b3465face9a3d2f1a21868f4b155
Gatsbeau <jabriel0ne@gmail.com>
2023-11-19 17:09:11 -0500

    Added model associations

--- models/index.js
+++ models/index.js
@@ -1,16 +1,29 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tag_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id",
+});
 
 module.exports = {
   Product,
